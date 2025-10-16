a64875fd709a54169b81e09bbde4446e5642a335
iclickfreedownloads <barca.rusch@gmail.com>
2025-10-16 11:38:27 -0400

    Import photo analysis tools in index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { photoAnalysisTools, handlePhotoAnalysisTool } from './photoTools';
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import {
